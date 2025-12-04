2698a5d2bbd8fcaf842bf2262536eb1c7559ea30
Darrzej <kastratidarsej@gmail.com>
2025-12-04 20:03:48 +0100

    a

---
+++ example2.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+
+html_content = """
+<html>
+<head>
+    <title>Example Page</title>
+</head>
+<body>
+    <h1>Welcome to Beautiful Soup</h1>
+    <p class="intro">Beautiful Soup makes web scraping easy</p>
+    <div id="content">
+    <p>Here are some links:</p>
+    <a href="http://example.com/page1">Link 1</a>
+    <a href="http://example.com/page2">Link 2</a>
+    <a href="http://example.com/page3">Link 3</a>
+    </div>
+    </body>
+    </html>
+"""
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+print("Title of the page:", soup.title.text)
+
+intro_text = soup.find('p', class_='intro').text
+print("Intro text:", intro_text)
